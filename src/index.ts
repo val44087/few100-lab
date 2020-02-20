d1216c98b2ddd883b386c33700ec4f83507c3de3
val44087 <61064107+val44087@users.noreply.github.com>
2020-02-20 17:11:42 +0000

    Lab100 - Day one

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,72 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+const calcBtn = document.getElementById('calcBtn') as HTMLInputElement;
+const calcBtn1 = document.getElementById('calcBtn1') as HTMLInputElement;
+const calcBtn2 = document.getElementById('calcBtn2') as HTMLInputElement;
+const calcBtn3 = document.getElementById('calcBtn3') as HTMLInputElement;
+const itemToCalc = document.getElementById('itemToCalc') as HTMLInputElement;
+const billAmount = document.getElementById('billAmount') as HTMLSpanElement;
+const tipPercent = document.getElementById('tipPercent') as HTMLSpanElement;
+const totalBill = document.getElementById('totalBill') as HTMLSpanElement;
+const list = document.getElementById('list') as HTMLUListElement;
+const percent = document.getElementById('percent') as HTMLSpanElement;
+const count = document.getElementById('count') as HTMLSpanElement;
+let calcedValue: number = 0
+const storedItems = localStorage.getItem('shopping-list');
+const items = itemToCalc.valueAsNumber;
+
+
+
+function updateCount() {
+    count.innerText = calcedValue.toString();
+    billAmount.innerText = itemToCalc.value;
+
+}
+//calcBtn.addEventListener('click', function () {
+//   var that = this as HTMLButtonElement;
+//  const tip = parseFloat(that.dataset.tip);
+//console.log(tip);
+
+//calcedValue = itemToCalc.valueAsNumber * tip;
+//console.log("Calced Value: " + calcedValue)
+
+//tipPercent.innerText = (tip * 100).toString();
+//percent.innerText = (tip * 100).toString();
+//count.innerText = calcedValue.toString();
+//billAmount.innerText = itemToCalc.value;
+//totalBill.innerText = (tip + itemToCalc.valueAsNumber).toString();
+//});
+
+
+calcBtn1.addEventListener('click', addTheItem);
+calcBtn2.addEventListener('click', addTheItem);
+calcBtn3.addEventListener('click', addTheItem);
+
+itemToCalc.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+        addTheItem();
+    }
+});
+
+interface ShoppingItem {
+    description: string;
+}
+
+function addTheItem() {
+    var that = this as HTMLButtonElement;
+    const tip = parseFloat(that.dataset.tip);
+    console.log(tip);
+
+    calcedValue = itemToCalc.valueAsNumber * tip;
+    console.log("Calced Value: " + calcedValue)
+
+    tipPercent.innerText = (tip * 100).toString();
+    percent.innerText = (tip * 100).toString();
+    count.innerText = calcedValue.toString();
+    billAmount.innerText = itemToCalc.value;
+    totalBill.innerText = (tip + itemToCalc.valueAsNumber).toString();
+    // saveIt();
+    // updateCount();
+}
+
 
