9a3f5c476437803321a583e8df1b32977b113070
val44087 <61064107+val44087@users.noreply.github.com>
2020-02-21 17:56:37 +0000

    Updates to Lab

--- src/index.ts
+++ src/index.ts
@@ -3,56 +3,46 @@ import './styles.css';
 const calcBtn1 = document.getElementById('calcBtn1') as HTMLInputElement;
 const calcBtn2 = document.getElementById('calcBtn2') as HTMLInputElement;
 const calcBtn3 = document.getElementById('calcBtn3') as HTMLInputElement;
-const itemToCalc = document.getElementById('itemToCalc') as HTMLInputElement;
+const amountToCalc = document.getElementById('amountToCalc') as HTMLInputElement;
 const billAmount = document.getElementById('billAmount') as HTMLSpanElement;
 const tipPercent = document.getElementById('tipPercent') as HTMLSpanElement;
 const totalBill = document.getElementById('totalBill') as HTMLSpanElement;
-// const list = document.getElementById('list') as HTMLUListElement;
 const percent = document.getElementById('percent') as HTMLSpanElement;
-const count = document.getElementById('count') as HTMLSpanElement;
-let calcedValue = 0;
-// const storedItems = localStorage.getItem('shopping-list');
-// const items = itemToCalc.valueAsNumber;
+const tipAmount = document.getElementById('tipAmount') as HTMLSpanElement;
+let calcedTipAmt = 0;
 
-function updateCount() {
-    count.innerText = calcedValue.toString();
-    billAmount.innerText = itemToCalc.value;
-}
+// Ask Eric how to get this to work.  dynmaic updating.
+// document.getElementById('amountToCalc').addEventListener('input', calcTheBill);
+document.getElementById('calcBtn1').addEventListener('click', calcTheBill);
+document.getElementById('calcBtn2').addEventListener('click', calcTheBill);
+document.getElementById('calcBtn3').addEventListener('click', calcTheBill);
+// Ask about currency formatting also
+// Which way is better??
+// calcBtn1.addEventListener('click', calcTheBill);
+// calcBtn2.addEventListener('click', calcTheBill);
+// calcBtn3.addEventListener('click', calcTheBill);
 
-calcBtn1.addEventListener('click', addTheItem);
-calcBtn2.addEventListener('click', addTheItem);
-calcBtn3.addEventListener('click', addTheItem);
 
-interface ShoppingItem {
-    description: string;
-}
-function addTheItem() {
+function calcTheBill() {
     const that = this as HTMLButtonElement;
+    if (amountToCalc.valueAsNumber < 0) {
+        amountToCalc.valueAsNumber = 0;
+        // make the border red here
+    }
     const tip = parseFloat(that.dataset.tip);
     const btnId = that.id;
-    console.log(tip);
-    calcedValue = itemToCalc.valueAsNumber * tip;
-    console.log('Calced Value: ' + calcedValue);
+    calcedTipAmt = amountToCalc.valueAsNumber * tip;
+
     tipPercent.innerText = (tip * 100).toFixed(2).toString();
     percent.innerText = (tip * 100).toString();
-    count.innerText = calcedValue.toFixed(2).toString();
-    billAmount.innerText = itemToCalc.value;
-    totalBill.innerText = (calcedValue + itemToCalc.valueAsNumber).toString();
+    tipAmount.innerText = calcedTipAmt.toFixed(2).toString();
+
+    billAmount.innerText = amountToCalc.value;
+    totalBill.innerText = (calcedTipAmt + amountToCalc.valueAsNumber).toString();
     disableButtons(btnId);
 }
+
 function disableButtons(btnId: string) {
-
-    // const btnList = ['calcBtn1', 'calcBtn2'];
-
-    // btnList.forEach(workButtons);
-
-
-    // function myFunction(item) {
-    //   sum += item;
-    //  document.getElementById('demo').innerHTML = sum;
-    // }
-
-
     if (btnId === 'calcBtn1') {
         calcBtn1.disabled = true;
         calcBtn2.disabled = false;
