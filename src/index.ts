7a1b94be2e5043570b266f5bda5214e447ca93b7
val44087 <61064107+val44087@users.noreply.github.com>
2020-03-06 13:50:20 +0000

    Got the buttons to work

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import './styles.css';
+// document.getElementById('amountToCalc').oninput = calcTheBill;
 
-const calcBtn1 = document.getElementById('calcBtn1') as HTMLInputElement;
-const calcBtn2 = document.getElementById('calcBtn2') as HTMLInputElement;
-const calcBtn3 = document.getElementById('calcBtn3') as HTMLInputElement;
+// I can get rid of these once I disable correctly
+// const calcBtn1 = document.getElementById('calcBtn1') as HTMLInputElement;
+// const calcBtn2 = document.getElementById('calcBtn2') as HTMLInputElement;
+// const calcBtn3 = document.getElementById('calcBtn3') as HTMLInputElement;
+
 const amountToCalc = document.getElementById('amountToCalc') as HTMLInputElement;
 const billAmount = document.getElementById('billAmount') as HTMLSpanElement;
 const tipPercent = document.getElementById('tipPercent') as HTMLSpanElement;
@@ -11,55 +14,70 @@ const percent = document.getElementById('percent') as HTMLSpanElement;
 const tipAmount = document.getElementById('tipAmount') as HTMLSpanElement;
 let calcedTipAmt = 0;
 
+
+const buttons = document.querySelectorAll('.btn') as NodeListOf<HTMLInputElement>;
+
 // Ask Eric how to get this to work.  dynmaic updating.
 // document.getElementById('amountToCalc').addEventListener('input', calcTheBill);
 document.getElementById('calcBtn1').addEventListener('click', calcTheBill);
 document.getElementById('calcBtn2').addEventListener('click', calcTheBill);
 document.getElementById('calcBtn3').addEventListener('click', calcTheBill);
 // Ask about currency formatting also
-// Which way is better??
-// calcBtn1.addEventListener('click', calcTheBill);
-// calcBtn2.addEventListener('click', calcTheBill);
-// calcBtn3.addEventListener('click', calcTheBill);
-
 
 function calcTheBill() {
-    const that = this as HTMLButtonElement;
-    if (amountToCalc.valueAsNumber < 0) {
-        amountToCalc.valueAsNumber = 0;
-        // make the border red here
-    }
-    const tip = parseFloat(that.dataset.tip);
-    const btnId = that.id;
-    calcedTipAmt = amountToCalc.valueAsNumber * tip;
+  const that = this as HTMLButtonElement;
 
-    tipPercent.innerText = (tip * 100).toFixed(2).toString();
-    percent.innerText = (tip * 100).toString();
-    tipAmount.innerText = calcedTipAmt.toFixed(2).toString();
+  if (amountToCalc.valueAsNumber < 0) {
+    amountToCalc.valueAsNumber = 0;
+    // make the border red here
+  }
+  const tip = parseFloat(that.dataset.tip);
+  const btnId = that.id;
 
-    billAmount.innerText = amountToCalc.value;
-    totalBill.innerText = (calcedTipAmt + amountToCalc.valueAsNumber).toString();
-    disableButtons(btnId);
+  calcedTipAmt = amountToCalc.valueAsNumber * tip;
+
+  tipPercent.innerText = (tip * 100).toFixed(2).toString();
+  percent.innerText = (tip * 100).toString();
+  tipAmount.innerText = calcedTipAmt.toFixed(2).toString();
+
+  billAmount.innerText = amountToCalc.value;
+  totalBill.innerText = (calcedTipAmt + amountToCalc.valueAsNumber).toString();
+  disableBTN(btnId);
+  // disableButtons(btnId);
 }
 
-function disableButtons(btnId: string) {
-    if (btnId === 'calcBtn1') {
-        calcBtn1.disabled = true;
-        calcBtn2.disabled = false;
-        calcBtn3.disabled = false;
+function disableBTN(btnId: string) {
+  // const bt = btnId;
+  buttons.forEach(x => {
+    if (x.id === btnId) {
+      x.setAttribute('disabled', 'disabled');
+
     } else {
-        if (btnId === 'calcBtn2') {
-            calcBtn2.disabled = true;
-            calcBtn1.disabled = false;
-            calcBtn3.disabled = false;
-        } else {
-            if (btnId === 'calcBtn3') {
-                calcBtn3.disabled = true;
-                calcBtn1.disabled = false;
-                calcBtn2.disabled = false;
-            }
-        }
+      x.removeAttribute('disabled');
     }
+  });
 }
 
 
+
+
+
+// function disableButtons(btnId: string) {
+  //    if (btnId === 'calcBtn1') {
+    //    calcBtn1.disabled = true;
+      //  calcBtn2.disabled = false;
+        // calcBtn3.disabled = false;
+      // } else {
+        // if (btnId === 'calcBtn2') {
+          // calcBtn2.disabled = true;
+        //  calcBtn1.disabled = false;
+          // calcBtn3.disabled = false;
+        // } else {
+          // if (btnId === 'calcBtn3') {
+            // calcBtn3.disabled = true;
+            // calcBtn1.disabled = false;
+            // calcBtn2.disabled = false;
+         // }
+        // }
+     // }
+    // };
