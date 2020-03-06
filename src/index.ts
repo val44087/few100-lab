68defe8c01c1b80b00a75b4287b4f4ce86ce0eb6
val44087 <61064107+val44087@users.noreply.github.com>
2020-03-06 18:02:37 +0000

    added storage

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 import './styles.css';
-// document.getElementById('amountToCalc').oninput = calcTheBill;
-
-// I can get rid of these once I disable correctly
-// const calcBtn1 = document.getElementById('calcBtn1') as HTMLInputElement;
-// const calcBtn2 = document.getElementById('calcBtn2') as HTMLInputElement;
-// const calcBtn3 = document.getElementById('calcBtn3') as HTMLInputElement;
 
 const amountToCalc = document.getElementById('amountToCalc') as HTMLInputElement;
 const billAmount = document.getElementById('billAmount') as HTMLSpanElement;
@@ -14,22 +8,25 @@ const percent = document.getElementById('percent') as HTMLSpanElement;
 const tipAmount = document.getElementById('tipAmount') as HTMLSpanElement;
 let calcedTipAmt = 0;
 
-
+const storedBtn = localStorage.getItem('calcBtn');
+const storedTip = localStorage.getItem('calcTip');
+document.getElementById('amountToCalc').oninput = calcTheBill;
 const buttons = document.querySelectorAll('.btn') as NodeListOf<HTMLInputElement>;
 
 // Ask Eric how to get this to work.  dynmaic updating.
-// document.getElementById('amountToCalc').addEventListener('input', calcTheBill);
 document.getElementById('calcBtn1').addEventListener('click', calcTheBill);
 document.getElementById('calcBtn2').addEventListener('click', calcTheBill);
 document.getElementById('calcBtn3').addEventListener('click', calcTheBill);
-// Ask about currency formatting also
+
+
 
 function calcTheBill() {
+
   const that = this as HTMLButtonElement;
 
   if (amountToCalc.valueAsNumber < 0) {
     amountToCalc.valueAsNumber = 0;
-    // make the border red here
+    // make; the; border; red; here;
   }
   const tip = parseFloat(that.dataset.tip);
   const btnId = that.id;
@@ -43,41 +40,19 @@ function calcTheBill() {
   billAmount.innerText = amountToCalc.value;
   totalBill.innerText = (calcedTipAmt + amountToCalc.valueAsNumber).toString();
   disableBTN(btnId);
-  // disableButtons(btnId);
+  saveIt(btnId, tip);
 }
 
 function disableBTN(btnId: string) {
-  // const bt = btnId;
   buttons.forEach(x => {
     if (x.id === btnId) {
       x.setAttribute('disabled', 'disabled');
-
     } else {
       x.removeAttribute('disabled');
     }
   });
 }
-
-
-
-
-
-// function disableButtons(btnId: string) {
-  //    if (btnId === 'calcBtn1') {
-    //    calcBtn1.disabled = true;
-      //  calcBtn2.disabled = false;
-        // calcBtn3.disabled = false;
-      // } else {
-        // if (btnId === 'calcBtn2') {
-          // calcBtn2.disabled = true;
-        //  calcBtn1.disabled = false;
-          // calcBtn3.disabled = false;
-        // } else {
-          // if (btnId === 'calcBtn3') {
-            // calcBtn3.disabled = true;
-            // calcBtn1.disabled = false;
-            // calcBtn2.disabled = false;
-         // }
-        // }
-     // }
-    // };
+function saveIt(btnId: string, tip: number) {
+  localStorage.setItem('calcBtn', JSON.stringify(btnId));
+  localStorage.setItem('calcTip', JSON.stringify(tip));
+}
