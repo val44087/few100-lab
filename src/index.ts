9e30289b21af4afb929ba894cda84f89c7a08136
val44087 <61064107+val44087@users.noreply.github.com>
2020-03-09 17:05:07 +0000

    Got the dynamic cals to work

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,55 @@ const totalBill = document.getElementById('totalBill') as HTMLSpanElement;
 const percent = document.getElementById('percent') as HTMLSpanElement;
 const tipAmount = document.getElementById('tipAmount') as HTMLSpanElement;
 let calcedTipAmt = 0;
+const tipStored = 0;
 
-const storedBtn = localStorage.getItem('calcBtn');
-const storedTip = localStorage.getItem('calcTip');
-document.getElementById('amountToCalc').oninput = calcTheBill;
+document.getElementById('amountToCalc').oninput = calcStoredTip;
+
 const buttons = document.querySelectorAll('.btn') as NodeListOf<HTMLInputElement>;
 
 // Ask Eric how to get this to work.  dynmaic updating.
-document.getElementById('calcBtn1').addEventListener('click', calcTheBill);
-document.getElementById('calcBtn2').addEventListener('click', calcTheBill);
-document.getElementById('calcBtn3').addEventListener('click', calcTheBill);
+document.getElementById('calcBtn1').addEventListener('click', calcTip);
+document.getElementById('calcBtn2').addEventListener('click', calcTip);
+document.getElementById('calcBtn3').addEventListener('click', calcTip);
+
+function calcStoredTip() {
+  const storedTip = localStorage.getItem('calcTip');
+  const storedBtn = localStorage.getItem('calcBtn');
 
 
-
-function calcTheBill() {
-
-  const that = this as HTMLButtonElement;
-
-  if (amountToCalc.valueAsNumber < 0) {
-    amountToCalc.valueAsNumber = 0;
-    // make; the; border; red; here;
+  if (storedTip) {
+    const tipStored = JSON.parse(storedTip);
+    const btnStored = JSON.parse(storedBtn);
+    calcTheBill(tipStored, btnStored);
+  } else {
+    if (NaN) {
+      tipPercent.innerText = null;
+      billAmount.innerText = null;
+      tipAmount.innerText = null;
+      totalBill.innerHTML = null;
+    }
   }
+}
+
+function calcTip() {
+  const that = this as HTMLButtonElement;
   const tip = parseFloat(that.dataset.tip);
   const btnId = that.id;
+  calcTheBill(tip, btnId);
+}
+
+
+function calcTheBill(tip: number, btnId: string) {
+
+  // const that = this as HTMLButtonElement;
+
+  if (amountToCalc.valueAsNumber === 0) {
+    amountToCalc.valueAsNumber = 0;
+    alert('Value cant be empty');
+    amountToCalc.style.borderColor = 'red';
+  }
+  // const tip = parseFloat(that.dataset.tip);
+  // const btnId = that.id;
 
   calcedTipAmt = amountToCalc.valueAsNumber * tip;
 
@@ -40,7 +66,7 @@ function calcTheBill() {
   billAmount.innerText = amountToCalc.value;
   totalBill.innerText = (calcedTipAmt + amountToCalc.valueAsNumber).toString();
   disableBTN(btnId);
-  saveIt(btnId, tip);
+  saveIt(tip, btnId);
 }
 
 function disableBTN(btnId: string) {
@@ -52,7 +78,7 @@ function disableBTN(btnId: string) {
     }
   });
 }
-function saveIt(btnId: string, tip: number) {
+function saveIt(tip: number, btnId: string) {
   localStorage.setItem('calcBtn', JSON.stringify(btnId));
   localStorage.setItem('calcTip', JSON.stringify(tip));
 }
